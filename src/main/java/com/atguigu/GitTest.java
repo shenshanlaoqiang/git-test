3a3c582116b23bda3f853dff5b34107f71069978
shenshanlaoqiang <53212532+shenshanlaoqiang@users.noreply.github.com>
2022-11-03 12:45:57 +0800

    pull test

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("push ssh!");
+        System.out.println("pull test!");
     }
 }
